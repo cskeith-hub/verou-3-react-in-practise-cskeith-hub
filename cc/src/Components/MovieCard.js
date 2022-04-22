fff0fddb5d46e1df48dee6131e133d371a03bde3
unknown <cskeith01@gmail.com>
2022-04-22 11:46:40 +0200

    Install uuidv4

--- cc/src/Components/MovieCard.js
+++ cc/src/Components/MovieCard.js
@@ -1,56 +1,64 @@
-import React from 'react'
+import {useState, useEffect } from "react";
+import apiKey from "../Config";
+import { v4 as uuidv4 } from 'uuid';
 import { Box, Image, Badge, StarIcon } from '@chakra-ui/react'
 
 function MovieCard() {
-    const property = {
-        imageUrl: 'https://bit.ly/2Z4KKcF',
-        imageAlt: 'Rear view of modern home with pool',
-        beds: 3,
-        baths: 2,
-        title: 'Modern home in city center in the heart of historic Los Angeles',
-        formattedPrice: '$1,900.00',
-        reviewCount: 34,
-        rating: 4,
-      }
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/top_rated?api_key='+ apiKey.movieKey)
+         .then((data) => data.json())
+         .then(data => {
+            setMovies(data.results)
+         })
+       }, []);
+       console.log(movies)
   return (
-    <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-    <Image src={property.imageUrl} alt={property.imageAlt} />
+    <>
+     {movies.map(movie => (
+        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+        <Image src={"https://image.tmdb.org/t/p/w500/"+movie.poster_path} alt="Movie Picture" />
+        <Box p='6'>
+        <Box display='flex' alignItems='baseline'>
+            <Badge borderRadius='full' px='2' colorScheme='teal'>
+                {movie.release_date}
+            </Badge>
+            <Box
+            color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'
+            >
+            {movie.vote_average} Vote * &bull; {movie.original_language} Original Language
+            </Box>
+        </Box>
 
-    <Box p='6'>
-      <Box display='flex' alignItems='baseline'>
-        <Badge borderRadius='full' px='2' colorScheme='teal'>
-          Name
-        </Badge>
         <Box
-          color='gray.500'
-          fontWeight='semibold'
-          letterSpacing='wide'
-          fontSize='xs'
-          textTransform='uppercase'
-          ml='2'
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            isTruncated
         >
-          {property.beds} beds &bull; {property.baths} baths
+            {movie.title}
         </Box>
-      </Box>
 
-      <Box
-        mt='1'
-        fontWeight='semibold'
-        as='h4'
-        lineHeight='tight'
-        isTruncated
-      >
-        {property.title}
-      </Box>
-
-      <Box>
-        {property.formattedPrice}
-        <Box as='span' color='gray.600' fontSize='sm'>
-          / wk
+        <Box>
+            {movie.overview}
+            <Box as='span' color='gray.600' fontSize='sm'>
+            / wk
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  </Box>
+        </Box>
+        </Box>
+
+     ))}
+ 
+    </>
+   
+ 
   )
 }
 
